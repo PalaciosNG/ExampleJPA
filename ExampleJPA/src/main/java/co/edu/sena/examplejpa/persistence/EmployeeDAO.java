74aada1ab8174373d028d7494cf97714c8365c91
PalaciosNG <nicolaspalaciosgomez@gmail.com>
2025-04-24 08:03:04 -0500

    Creación de proyecto, capa de modelo y persistencia
    modelo con jpa y persistencia con Dao's

---
+++ ExampleJPA/src/main/java/co/edu/sena/examplejpa/persistence/EmployeeDAO.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.sena.examplejpa.persistence;
+
+import co.edu.sena.examplejpa.model.Employee;
+import java.util.List;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Nicol
+ */
+public class EmployeeDAO implements IEmployeeDAO{
+
+    @Override
+    public void insert(Employee employee) throws Exception {
+        try 
+        {
+            EntityManagerHelper.getEntityManager().persist(employee);
+        } 
+        catch (RuntimeException e) 
+        {
+            throw e;
+        }
+    }
+
+    @Override
+    public void update(Employee employee) throws Exception {
+        try 
+        {
+            EntityManagerHelper.getEntityManager().merge(employee);
+        } 
+        catch (RuntimeException e) 
+        {
+            throw e;
+        }
+    }
+
+    @Override
+    public void delete(Employee employee) throws Exception {
+        try 
+        {
+            EntityManagerHelper.getEntityManager().remove(employee);
+        } 
+        catch (RuntimeException e) 
+        {
+            throw e;
+        }
+    }
+
+    @Override
+    public Employee findById(Long id) throws Exception {
+        try 
+        {
+            return EntityManagerHelper.getEntityManager().find(Employee.class, id);
+        } 
+        catch (RuntimeException e) 
+        {
+            throw e;
+        }
+    }
+
+    @Override
+    public List<Employee> findAll() throws Exception {
+        try 
+        {
+            Query query = (Query) EntityManagerHelper.getEntityManager()
+                    .createNamedQuery("Employee.findAll");
+            return query.getResultList();
+        } 
+        catch (RuntimeException e) 
+        {
+            throw e;
+        } 
+    }
+}
